a18b664e2082f254ee6fad579b15c94bd7bfc10b
Armen Zambrano G <armenzg@sentry.io>
2022-04-06 13:58:23 -0400

    Fix test

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -1,6 +1,6 @@
-from src.trace import generate_trace
+from src.trace import send_trace
 from .fixtures import *
 
 
 def test_workflow_without_steps(skipped_workflow):
-    assert generate_trace(skipped_workflow) == None
+    assert send_trace(skipped_workflow) == None
