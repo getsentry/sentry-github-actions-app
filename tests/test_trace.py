145a17dd7e7df0d9e28a49b92b29ffeec9299649
Armen Zambrano G <armenzg@sentry.io>
2022-04-28 08:55:16 -0400

    More changes

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -1,4 +1,4 @@
-from unittest import mock
+from unittest.mock import patch, MagicMock
 
 import responses
 from freezegun import freeze_time
@@ -7,25 +7,31 @@ from src.trace import _base_transaction, send_trace, _generate_trace
 from .fixtures import *
 
 
-# XXX: In _base_transaction we call it 3 times, thus, it should really be returning
-# three different values
+hex_items = [
+    "06a005dd99324b5e8b7f874be7d41568",
+    "6cd650677285431b8c68a150dcb76cbc",
+    "a6eefe13d6a64c8992e5e47c66a21432",
+]
+
+
 class UUID:
-    hex = "06a005dd99324b5e8b7f874be7d41568"
+    def __init__(self, value):
+        self.hex = value
 
 
-@mock.patch("src.trace.uuid.uuid4")
+@patch("src.trace.uuid.uuid4")
 def test_base_transaction(mock_uuid):
-    mock_uuid.return_value = UUID
+    mock_uuid.return_value = UUID(hex_items[0])
 
     assert _base_transaction() == {
         "contexts": {
             "trace": {
-                "span_id": UUID.hex[16:],
-                "trace_id": UUID.hex,
+                "span_id": hex_items[0][16:],
+                "trace_id": hex_items[0],
                 "type": "trace",
             }
         },
-        "event_id": UUID.hex,
+        "event_id": hex_items[0],
         "transaction": "default",
         "type": "transaction",
         "user": {},
@@ -37,11 +43,12 @@ def test_workflow_without_steps(skipped_workflow):
     assert send_trace(skipped_workflow) == None
 
 
-@mock.patch("src.trace.uuid.uuid4")
 @freeze_time()
 @responses.activate
+@patch("src.trace.uuid.uuid4")
 def test_workflow_basic_test(mock_uuid, jobA_job, jobA_runs, jobA_workflow, jobA_trace):
-    mock_uuid.return_value = UUID
+    # XXX: This is not quite right as there will be three calls to it and each should be different
+    mock_uuid.return_value = UUID(hex_items[0])
     responses.get(
         "https://api.github.com/repos/getsentry/sentry/actions/runs/2104746951",
         json=jobA_runs,
