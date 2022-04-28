a039bba8448772ab37203088bf40bb0893418cfe
Armen Zambrano G <armenzg@sentry.io>
2022-04-28 08:57:12 -0400

    More changes

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -7,11 +7,7 @@ from src.trace import _base_transaction, send_trace, _generate_trace
 from .fixtures import *
 
 
-hex_items = [
-    "06a005dd99324b5e8b7f874be7d41568",
-    "6cd650677285431b8c68a150dcb76cbc",
-    "a6eefe13d6a64c8992e5e47c66a21432",
-]
+HEX = "06a005dd99324b5e8b7f874be7d41568"
 
 
 class UUID:
@@ -21,17 +17,18 @@ class UUID:
 
 @patch("src.trace.uuid.uuid4")
 def test_base_transaction(mock_uuid):
-    mock_uuid.return_value = UUID(hex_items[0])
+    # XXX: Calling this should give us different values each time
+    mock_uuid.return_value = UUID(HEX)
 
     assert _base_transaction() == {
         "contexts": {
             "trace": {
-                "span_id": hex_items[0][16:],
-                "trace_id": hex_items[0],
+                "span_id": HEX[16:],
+                "trace_id": HEX,
                 "type": "trace",
             }
         },
-        "event_id": hex_items[0],
+        "event_id": HEX,
         "transaction": "default",
         "type": "transaction",
         "user": {},
@@ -47,8 +44,8 @@ def test_workflow_without_steps(skipped_workflow):
 @responses.activate
 @patch("src.trace.uuid.uuid4")
 def test_workflow_basic_test(mock_uuid, jobA_job, jobA_runs, jobA_workflow, jobA_trace):
-    # XXX: This is not quite right as there will be three calls to it and each should be different
-    mock_uuid.return_value = UUID(hex_items[0])
+    # XXX: Calling this should give us different values each time
+    mock_uuid.return_value = UUID(HEX)
     responses.get(
         "https://api.github.com/repos/getsentry/sentry/actions/runs/2104746951",
         json=jobA_runs,
