0e9955b98d1e84eefd2d19ab0a06a32606905efe
Armen Zambrano G <armenzg@sentry.io>
2022-05-11 14:46:18 -0400

    feat: Add Sentry's Flask integration (#18)
    This is in hopes that it makes investigating real issues easier since the headers and the payload are missing.

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ import sentry_sdk
 from flask import jsonify, request, Flask
 from sentry_sdk import capture_exception
 
+from sentry_sdk.integrations.flask import FlaskIntegration
+
 from .handle_event import handle_event
 
 APP_DSN = os.environ.get("APP_DSN")
@@ -12,6 +14,7 @@ if APP_DSN:
     # This tracks errors and performance of the app itself rather than GH workflows
     sentry_sdk.init(
         dsn=APP_DSN,
+        integrations=[FlaskIntegration()],
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production.
@@ -36,3 +39,16 @@ def main():
         logger.exception(e)
         capture_exception(e)
         return jsonify({"reason": "There was an error."}), 500
+
+
+if os.environ.get("FLASK_ENV") == "development":
+
+    @app.route("/debug-sentry")
+    def trigger_error():
+        try:
+            1 / 0
+        except Exception as e:
+            # Report it to Sentry
+            capture_exception(e)
+            # Let Flask handle the rest
+            raise e
