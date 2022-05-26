b5f34c45980c98d39fa8145ee905335bef56ad6e
Armen <armenzg@sentry.io>
2022-05-26 12:57:15 -0400

    WIP

--- src/github_app.py
+++ src/github_app.py
@@ -1,41 +1,44 @@
 import os
-from datetime import datetime as dt
+from datetime import datetime as dt, timedelta
 
 import jwt
 import requests
 
 
-def foo():
-    # Open the file as f.
-    # The function readlines() reads the file.
-    with open("sentry-github-app-prototype.2022-05-25.private-key.pem", "rb") as f:
-        private_key = f.readlines()
+def get_private_key(file_path=None, gcp_secret=None):
+    assert file_path or gcp_secret
+    private_key = None
+    if file_path:
+        # Open the file as f.
+        # The function readlines() reads the file.
+        with open(file_path, "rb") as f:
+            private_key = f.read()
+    else:
+        raise NotImplemented()
+    return private_key
 
+
+def get_jwt_token(private_key):
     payload = {
         # issued at time, 60 seconds in the past to allow for clock drift
-        "iat": dt.utcnow() - dt.timedelta(minutes=1),
+        "iat": dt.utcnow() - timedelta(minutes=1),
         # JWT expiration time (10 minute maximum)
-        "exp": dt.utcnow() + dt.timedelta(minutes=10),
+        "exp": dt.utcnow() + timedelta(minutes=10),
         # GitHub App's identifier
         "iss": os.environ["GH_APP_ID"],
     }
 
-    token = jwt.encode(payload, private_key, algorithm="RS256")
+    return jwt.encode(payload, private_key, algorithm="RS256")
 
-    # $ curl -i -H "Authorization: Bearer YOUR_JWT" -H "Accept: application/vnd.github.v3+json" https://api.github.com/app
+
+def get_org_for_token(token):
     headers = {
         "Accept": "application/vnd.github.v3+json",
         "Authorization": f"Bearer {token}",
     }
-
     req = requests.get("https://api.github.com/app", headers=headers)
-    import pdb
+    req.raise_for_status()
+    response = req.json()
 
-    pdb.set_trace()
-
-    # XXX: Where could I find the public key?
-    # jwt.decode(token, public_key, algorithms=["RS256"])
-
-
-if __name__ == "__main__":
-    foo()
+    # The second value tracks the org this token is useful for
+    return response["owner"]["login"]
--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from sentry_sdk import capture_exception
 
 from sentry_sdk.integrations.flask import FlaskIntegration
 
-from .github_app import foo
+from .github_app import generate_jwt_token, get_jwt_token, get_private_key
 from .event_handler import EventHandler
 
 APP_DSN = os.environ.get("APP_DSN")
@@ -27,20 +27,29 @@ LOGGING_LEVEL = os.environ.get("LOGGING_LEVEL", "INFO")
 logger = logging.getLogger(__name__)
 logger.setLevel(LOGGING_LEVEL)
 
-# We need an authorized token to fetch the API. If you have SSO on your org you will need to grant permission
-# Your app and the Github webhook will share this secret
-# You can create an .env file and place the token in it
-GH_TOKEN = os.environ.get("GH_TOKEN")
+
+ALLOWED_ORGS = "armenzg-dev"
+
+# Running as a Github App
+if os.environ.get("GH_APP_ID"):
+    # XXX: only for dev mode
+    file_path = os.environ["GH_PRIVATE_KEY_PATH"]
+    TOKEN = get_jwt_token(get_private_key(file_path))
+elif os.environ.get("GH_TOKEN"):
+    # We need an authorized token to fetch the API. If you have SSO on your org you will need to grant permission
+    # Your app and the Github webhook will share this secret
+    # You can create an .env file and place the token in it
+    TOKEN = os.environ["GH_TOKEN"]
+
 # Where to report Github actions transactions
 SENTRY_GITHUB_DSN = os.environ.get("SENTRY_GITHUB_DSN")
 
 app = Flask(__name__)
-handler = EventHandler(token=GH_TOKEN, dsn=SENTRY_GITHUB_DSN)
+handler = EventHandler(token=TOKEN, dsn=SENTRY_GITHUB_DSN)
 
 
 @app.route("/", methods=["POST"])
 def main():
-    foo()
     if not handler.valid_signature(request.data, request.headers):
         abort(
             400,
