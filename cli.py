ea7766e0a8fa6d75987d5beb8de1f8c6faf9d4a0
Armen Zambrano G <armenzg@sentry.io>
2022-08-11 11:16:34 -0400

    WIP

--- cli.py
+++ cli.py
@@ -1,53 +1,53 @@
 from __future__ import annotations
 
-import json
 import logging
 import os
-import sys
 
 import requests
 
 from src.github_app import GithubAppToken
 from src.github_sdk import GithubClient
-from src.web_app_handler import init_config
+from src.sentry_config import fetch_dsn_for_github_org
+from src.web_app_handler import WebAppHandler
+
+# import argparse
+# import json
+
 
 logging.getLogger().setLevel(os.environ.get("LOGGING_LEVEL", "INFO"))
 logging.basicConfig()
 
 # Point this script to the URL of a job and we will trace it
 # You give us this https://github.com/getsentry/sentry/runs/5759197422?check_suite_focus=true
-# Or give it a path to a file with a webhook payload
-# e.g. tests/fixtures/jobA/job.json
+# Only support GH app and URL modes
+def _ingest_run(url):
+    _, _, _, org, repo, _, run_id = url.split("?")[0].split("/")
+    req = requests.get(
+        f"https://api.github.com/repos/{org}/{repo}/actions/jobs/{run_id}",
+    )
+    req.raise_for_status()
+    job = req.json()
+
+    web_app = WebAppHandler()
+    with GithubAppToken(**web_app.config.gh_app._asdict()).get_token() as token:
+        client = GithubClient(
+            token=token,
+            dsn=os.environ.get("SENTRY_GITHUB_DSN"),
+        )
+        client.send_trace(job)
+
+
+def _fetch_private_file(org):
+    web_app = WebAppHandler()
+
+    with GithubAppToken(**web_app.config.gh_app._asdict()).get_token() as token:
+        print(token)
+        fetch_dsn_for_github_org(org, token)
 
 
 def main():
-    argument = sys.argv[1]
-    token = None
-
-    if argument.startswith("https"):
-        _, _, _, org, repo, _, run_id = argument.split("?")[0].split("/")
-        req = requests.get(
-            f"https://api.github.com/repos/{org}/{repo}/actions/jobs/{run_id}",
-        )
-        req.raise_for_status()
-        job = req.json()
-    else:
-        with open(argument) as f:
-            job = json.load(f)
-        org = job["url"].split("/")[4]
-
-    config = init_config()
-    if os.environ.get("GH_APP_ID"):
-        app = GithubAppToken(**config.gh_app._asdict())
-        token = app.get_token()
-    else:
-        token = os.environ["GH_TOKEN"]
-
-    client = GithubClient(
-        token=token,
-        dsn=os.environ.get("SENTRY_GITHUB_DSN"),
-    )
-    client.send_trace(job)
+    # parser = argparse.ArgumentParser()
+    _fetch_private_file("armenzg-dev")
 
 
 if __name__ == "__main__":
