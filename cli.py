0a66e5631ca5cc5f5a6833e321062c0d3c8c8910
Armen <armenzg@sentry.io>
2022-08-11 14:00:36 -0400

    Make the cli work with dsn from repo

--- cli.py
+++ cli.py
@@ -9,6 +9,7 @@ import requests
 
 from src.github_app import GithubAppToken
 from src.github_sdk import GithubClient
+from src.sentry_config import fetch_dsn_for_github_org
 from src.web_app_handler import init_config
 
 logging.getLogger().setLevel(os.environ.get("LOGGING_LEVEL", "INFO"))
@@ -21,33 +22,29 @@ logging.basicConfig()
 
 
 def main():
-    argument = sys.argv[1]
+    url = sys.argv[1]
     token = None
 
-    if argument.startswith("https"):
-        _, _, _, org, repo, _, run_id = argument.split("?")[0].split("/")
-        req = requests.get(
-            f"https://api.github.com/repos/{org}/{repo}/actions/jobs/{run_id}",
-        )
-        req.raise_for_status()
-        job = req.json()
-    else:
-        with open(argument) as f:
-            job = json.load(f)
-        org = job["url"].split("/")[4]
+    _, _, _, org, repo, _, run_id = url.split("?")[0].split("/")
+    req = requests.get(
+        f"https://api.github.com/repos/{org}/{repo}/actions/jobs/{run_id}",
+    )
+    req.raise_for_status()
+    job = req.json()
 
     config = init_config()
-    if os.environ.get("GH_APP_ID"):
-        app = GithubAppToken(**config.gh_app._asdict())
-        token = app.get_token()
-    else:
-        token = os.environ["GH_TOKEN"]
-
-    client = GithubClient(
-        token=token,
-        dsn=os.environ.get("SENTRY_GITHUB_DSN"),
-    )
-    client.send_trace(job)
+    # When you install an APP under your org, the installation ID will be part of the url
+    # e.g. https://github.com/settings/installations/<foo>
+    # Visit https://github.com/settings/installations to find it
+    installation_id = os.environ["INSTALLATION_ID"]
+    with GithubAppToken(**config.gh_app._asdict()).get_token(installation_id) as token:
+        # Once the Sentry org has a .sentry repo we can remove the DSN from the deployment
+        dsn = fetch_dsn_for_github_org(org, token)
+        client = GithubClient(
+            token=token,
+            dsn=dsn,
+        )
+        client.send_trace(job)
 
 
 if __name__ == "__main__":
